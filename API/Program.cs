f680a8a4a0ba9cd4b693aef89096bb03123b3af1
Pooja Raj <poojar@cribsontech.onmicrosoft.com>
2023-11-20 17:21:26 +0530

    End of section3

--- API/Program.cs
+++ API/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +18,8 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(builder =>builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
