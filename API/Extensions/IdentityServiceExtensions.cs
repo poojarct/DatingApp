f83c042055d529b9e84f58171a676fbcbd2ae4a9
Pooja Raj <poojar@cribsontech.onmicrosoft.com>
2023-11-23 22:09:27 +0530

    clear error in IdentityServiceExtension

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -13,7 +13,7 @@ namespace API.Extensions
             options.TokenValidationParameters = new TokenValidationParameters
             {
             ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Config["TokenKey"])),
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
             ValidateIssuer = false,
             ValidateAudience = false 
             };
